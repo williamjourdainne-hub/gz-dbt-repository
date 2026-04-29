8c9b002fd9539b79d8d6be20c9d8de30bba2bb28
william.jourdainne@gmail.com <william.jourdainne@gmail.com>
2026-04-29 13:59:08 +0000

    modification schema and description + tests

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -11,11 +11,11 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
-        ship_cost
+        CAST (ship_cost AS INT64) AS ship_cost
 
     from source
+    
 
 )
 
